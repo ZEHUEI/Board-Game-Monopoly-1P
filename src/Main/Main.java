e3d7a0b1d6a1f0832d5f4e3df762f64936888eec
Ze Huei <zehueilim2005@gmail.com>
2025-06-18 13:02:17 +0100

    Screen

---
+++ src/Main/Main.java
@@ -0,0 +1,30 @@
+package Main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+//declare logo from resource folder
+        ImageIcon logo = new ImageIcon(".//res//call.png");
+        window.setTitle("JUST A PROGRAM");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        //set the icon
+        window.setIconImage(logo.getImage());
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        //rin game
+        gamePanel.setupGame();
+
+        gamePanel.startGameThread();
+    }
+}
